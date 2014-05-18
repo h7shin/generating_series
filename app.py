bbff6b40d15d8136d2abe906a728fef40b571b01
Hyunwook Shin <Hyunwook@hyunwookpc.(none)>
2014-05-18 14:32:38 -0400

    adding in basic files

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ app.config.update(
 # controllers
 @app.route("/")
 def hello():
-    output = "<html><body><textarea>"
+    output = "<html><body><textarea rows='50' cols='50'>"
     output += "In this example, W(I) = |I|, where I is a set of integers and I is a subset of S" + "\n"
     for n in range(1,5):
         output += "Enter a number n such that S =  {1...n}:  " + str(n) + "\n"
-    output += str(compute(create_list(n),n,'not_fixed')) + "\n"
+        output += str(compute(create_list(n),n,'not_fixed')) + "\n"
     output += "</textarea></body></html>" + "\n"
     return output
 # launch
