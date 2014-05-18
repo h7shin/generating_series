dd0034a5ca8541b23944611fbbd173fafc459e48
Hyunwook Shin <Hyunwook@hyunwookpc.(none)>
2014-05-18 14:35:01 -0400

    adding in basic files

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask
 from compute_local import compute
 from client import get_num_map_nodes, create_list
+import cgi          # for _GET variable
+
 
 # initialization
 app = Flask(__name__)
@@ -12,10 +14,12 @@ app.config.update(
 # controllers
 @app.route("/")
 def hello():
+    form = cgi.FieldStorage()
+    print (form["n"])
     output = "<html><body><textarea rows='50' cols='50'>"
     output += "In this example, W(I) = |I|, where I is a set of integers and I is a subset of S" + "\n"
     for n in range(1,5):
-        output += "Enter a number n such that S =  {1...n}:  " + str(n) + "\n"
+        output += "Auto Enter a number n such that S =  {1.." + str(n) + "}\n"
         output += str(compute(create_list(n),n,'not_fixed')) + "\n"
     output += "</textarea></body></html>" + "\n"
     return output
