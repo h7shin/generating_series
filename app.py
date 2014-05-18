2a9c296c3cbac5d1b98b637c17ec655591e4fcc7
Hyunwook Shin <Hyunwook@hyunwookpc.(none)>
2014-05-18 14:23:49 -0400

    adding in basic files

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask
+from compute_local import compute
+from client import get_num_map_nodes, create_list
 
 # initialization
 app = Flask(__name__)
@@ -10,9 +12,15 @@ app.config.update(
 # controllers
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return "Testing...."
+    print("W(I) = |I| where I is a set of integers and I is a subset of S")
+    for n in range(1,5):
+        print("Enter a number n such that S =  {1...n}:  ", n)
+        #compute(create_list(1),,'not_fixed')
 
 # launch
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+        
