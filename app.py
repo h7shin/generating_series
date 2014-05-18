0409d2322c269d34c70c62d6a84c520e73eadb32
Hyunwook Shin <Hyunwook@hyunwookpc.(none)>
2014-05-18 15:30:48 -0400

    adding in basic files

--- app.py
+++ app.py
@@ -2,30 +2,28 @@ import os
 from flask import Flask
 from compute_local import compute
 from client import get_num_map_nodes, create_list
-import cgi          # for _GET variable
 import time
 
-# initialization
+# Init
 app = Flask(__name__)
 app.config.update(
     DEBUG = True,
 )
 
-# controllers
+# Controller
 @app.route("/")
-def hello():
-    form = cgi.FieldStorage()
-    
+def runapp():
+     
     output = "<html><body><div style='font-family: Arial '>"
     output += "<h2>Generating Series Live Demonstration</h2>"
     output += "<br>"
     output += "<b>Plan, Source Code and Documentation: <a href='https://github.com/hyunwookshin/generating_series'>github/hyunwookshin/generating_series</b></a><br>"
     output += "Last edited: May 18, 2014<br>"
-    output +=" ".join(form)+ "<br>"
+    output += "<br>"
     output += "In this example, Let weight function W(I) = |I| (size of I), where I is a set of integers and I is a subset of S" + "<br>"
     output += "<img src='https://raw.githubusercontent.com/hyunwookshin/generating_series/master/equation/equation.png' width='500px'><br>"
     for n in 2,4,8:
-        
+        output += "<h3><b><i>n</i></b> = " + str(n) + "</h3><br>"
         output += "Automatically entered a number <i>n</i> = " + str(n) + " such that <b>S</b> =  <b>{1,..," + str(n) + "}</b><br>"
         output += "S = {" + ", ".join(str(i) for i in create_list(n))+ "} <br>"
         start_time = time.time() 
@@ -34,9 +32,11 @@ def hello():
         output += "<br>"
     output += "</div></body></html>" + "<br>"
     return output
-# launch
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
-        
+ # Credits
+ # Heroku Tutorial:
+ # Link: http://www.shea.io/lightweight-python-apps-with-flask-twitter-bootstrap-and-heroku/
